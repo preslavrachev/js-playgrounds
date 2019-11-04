fba99924096b84f755a8295ca8dfc274c3126df5
Preslav Rachev <rachev.preslav@gmail.com>
2019-11-04 09:03:42 +0100

    First commit

---
+++ svelte-bookmarks/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let name;
+
+  let bookmarks = {
+    "Go Programming": [{}]
+  };
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+{Parser}
